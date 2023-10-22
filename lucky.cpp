74b3b54fe4679035cc2f6f07d3caabf6883d9791
TruongSon0212 <148667129+TruongSon0212@users.noreply.github.com>
2023-10-22 10:00:04 +0700

    Add files via upload

---
+++ lucky.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> luckyNumbers = {13579, 26791, 26792, 33445, 55555, 62483, 77777, 79422, 85647, 93121};
+    int winningNumber;
+
+    sort(luckyNumbers.begin(), luckyNumbers.end());
+
+    cout << "Enter this week's winning 5-digit number: ";
+    cin >> winningNumber;
+
+    bool isWinner = std::binary_search(luckyNumbers.begin(), luckyNumbers.end(), winningNumber);
+
+    if (isWinner) {
+        std::cout << "Congratulations! You have a winning ticket!\n";
+    } else {
+        std::cout << "Sorry, none of your tickets are winners this week.\n";
+    }
+
+    return 0;
+}
